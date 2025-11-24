f9811f247f49596e126236974219d27f101bac8b
Leonardo Frederico de Paula <summerside@escoladotrabalhador40.com.br>
2025-11-24 01:00:22 -0300

    Update client.php

--- vendor/twilio/src/rest/client.php
+++ vendor/twilio/src/rest/client.php
@@ -1,1 +1,9 @@
+<?php
+namespace Twilio\Rest;
+class Client {
+    public function __construct($sid, $token) {
+        // Lightweight stub. Install real SDK with Composer for full functionality.
+    }
+}
+?>
 
